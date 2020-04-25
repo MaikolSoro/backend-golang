9c6ba2cb3fc847a7adc4734311c0e155bfb732a0
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-04-25 06:55:42 -0600

    Se creo la func para loguearse

---
+++ bd/intentoLogin.go
@@ -0,0 +1,23 @@
+package bd
+
+import (
+	"github.com/maikol/backend-golang/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+/*IntentoLogin realiza el chequeo de login a la BD */
+func IntentoLogin(email string, password string) (models.Usuario, bool) {
+
+	usu, encontrado, _ := ChequeoYaExisteUsuario(email)
+	if !encontrado {
+		return usu, false
+	}
+	passwordBytes := []byte(password)
+	passwordBD := []byte(usu.Password)
+	err := bcrypt.CompareHashAndPassword(passwordBD, passwordBytes)
+
+	if err != nil {
+		return usu, false
+	}
+	return usu, true
+}
